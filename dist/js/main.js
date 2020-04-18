e19c16ad32b92667e3c4d25cc941463769fa1491
Shaan Alam <48273777+shaan71845@users.noreply.github.com>
2020-04-18 07:32:40 +0530

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,38 @@
 const menuBtn = document.querySelector('.hamburger');
 const navLinks = document.querySelector(".nav-links");
 const lines = document.querySelectorAll(".line");
+const darkModeBtn = document.querySelector('#dark-mode');
+let darkMode = false;
 
 menuBtn.addEventListener('click', () => {
   navLinks.classList.toggle('open');
 
   lines.forEach(line => line.classList.toggle('close'))
 });
+
+
+darkModeBtn.addEventListener('click', e => {
+  e.preventDefault();
+  
+  if (!darkMode) {
+    document.body.classList.add('dark-body')    ;
+    darkModeBtn.innerHTML = 'Light Mode';
+    document.getElementById('aboutme').background = '../img/waves-dark.png';
+    darkMode = true;
+    localStorage.setItem('dark-mode', 'true');
+  } else {
+    document.body.classList.remove('dark-body')    ;
+    darkModeBtn.innerHTML =  'Dark Mode';
+    document.getElementById('aboutme').background = '../img/waves.png';
+    darkMode = false;
+    localStorage.setItem('dark-mode', 'false');
+  }
+});
+
+window.addEventListener('load', () => {
+  if (localStorage.getItem('dark-mode') && localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark-body', 'dark-body-open');
+  } else {
+    document.body.classList.remove('dark-body');
+  }
+})
